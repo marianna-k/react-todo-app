e856efc6b02f2fec632cfbefa28f930600bb486b
marianna-k <mariannakapa12@gmail.com>
2025-06-10 12:15:36 +0300

    feat: add toggle, completed task functionality

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -21,6 +21,7 @@ const todoReducer = (state: TodoProps[], action: Action): TodoProps[] => {
                 {
                     id: Date.now(),
                     text: action.payload,
+                    completed: false,
                 }
             ]
         case "DELETE":
@@ -31,6 +32,13 @@ const todoReducer = (state: TodoProps[], action: Action): TodoProps[] => {
                     ? {...todo, text: action.payload.newText}
                     : todo
             );
+
+        case "COMPLETE":
+                return state.map (todo =>
+                todo.id === action.payload ? {...todo, completed: !todo.completed}
+                : todo);
+                default:
+                    return state;
     }
 };
 
